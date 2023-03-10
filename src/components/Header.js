8b6f43c7cd7918abfbfd81f3e9886290490e88b7
Shuvam Gupta <gupta1999shuvam@gmail.com>
2023-03-10 23:11:28 +0530

    Added my keeper app project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import HighlightIcon from "@mui/icons-material/Highlight";
 
 export default function Header() {
   return (
     <header>
-      <h1 className="header h1">Keeper</h1>
+      <h1 className="header h1">
+        <HighlightIcon />
+        Keeper
+      </h1>
     </header>
   );
 }
